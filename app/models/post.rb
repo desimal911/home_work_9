ba1f17f467a704c457adbf4b7944805760f4ea2d
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2021-01-12 20:55:29 +0200

    add scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :images, as: :imageable
 
+  scope :published, -> { where.not(published_at: nil) }
+
   def publish
-    self.published_at = DateTime.now
-    self.save
+    update_attribute(:published_at, DateTime.now)
+  end
+
+  def unpublish
+    update_attribute(:published_at, nil)
   end
 end
