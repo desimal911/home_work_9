f7b30c6d4e8c03497298547257dca062a5e55591
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2021-01-12 21:23:02 +0200

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
 
   scope :adults, -> { where('birthday <= ?', 18.years.ago) }
 
-  validates :email, :uniqueness => true, format: { with: EMAIL_VALID }
+  validates :email, uniqueness: true, format: { with: EMAIL_VALID }, presence: true
+  validates :password, length: { minimum: 8 }, presence: true
+  validates :username, uniqueness: true, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
