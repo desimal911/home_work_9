f02fd2588760dab4ea9d3b5c683a26e2a2fa01e3
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2021-01-19 00:02:19 +0200

    HW_9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ApplicationRecord
   EMAIL_VALID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :images, as: :imageable
+  has_many :memberships
+  has_many :groups, through: :memberships
 
   scope :adults, -> { where('birthday <= ?', 18.years.ago) }
 
   validates :email, uniqueness: true, format: { with: EMAIL_VALID }, presence: true
   validates :password, length: { minimum: 8 }, presence: true
-  validates :username, uniqueness: true, presence: true
+  validates :personal_data, uniqueness: true, presence: true
+
 
   def full_name
     "#{first_name} #{last_name}"
